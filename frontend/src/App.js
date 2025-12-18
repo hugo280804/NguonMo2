279d9234abedcf585d8d74c550bc19721b06ce7d
Vo Anh Tuan <voanhtuan@gmail.com>
2025-12-18 12:45:40 +0700

    Initial project 1

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Frontend</h1>
+      <p>Project 1 - CI/CD with GitHub Actions</p>
+    </div>
+  );
+}
+
+export default App;
